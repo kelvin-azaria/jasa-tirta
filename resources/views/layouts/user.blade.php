6a4bbc3d0229e3ac9afd010202b34175451a86ed
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-09-11 19:33:49 +0700

    add users table and user layout file

---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <title>User Dashboard</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- Styles -->
+    <link rel="icon" href="{{ asset('img/yc-icon.png') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+  </head>
+  <body>
+    <div id="app">
+      <main>@yield('content')</main>
+    </div>
+
+    @yield('script')
+  </body>
+</html>
