db1fac01a68afa8d1baefc39886bbab2ceb7d29a
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-09-15 15:29:32 +0700

    resolve route conflict

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/test', [HomeController::class, 'test'])->name('test');
 Route::resource('dashboard', UserDashboardController::class)->only(['index','show']);
 
 Auth::routes([
