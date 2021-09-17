8a9f2ed940cbde4bad271443008bb221d3795a2f
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-09-17 11:01:45 +0700

    add admin login functionality

---
+++ resources/views/pages/admins_dashboard/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-10">
+      <h1>admin dashboard</h1>
+    </div>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LeaderboardController;
 use App\Http\Controllers\UserDashboardController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('faq', FaqController::class)->only(['index']);
@@ -25,3 +15,16 @@ Route::resource('dashboard', UserDashboardController::class)->only(['index','sho
 Auth::routes([
   'verify' => false,
 ]);
+
+//ADMIN AUTH ROUTES
+Route::prefix('/admin')->name('admin.')->group(function(){
+  Route::namespace('Auth')->group(function(){
+        
+    //Login Routes
+    Route::get('/login','\App\Http\Controllers\Admin\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('/login','\App\Http\Controllers\Admin\Auth\LoginController@login');
+    Route::post('/logout','\App\Http\Controllers\Admin\Auth\LoginController@logout')->name('logout');
+  });
+
+  Route::resource('dashboard', AdminDashboardController::class)->only(['index']);
+});
