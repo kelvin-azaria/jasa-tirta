f48ffdf7ac6d4429f342d61674d1bb234081a6ed
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-09-17 11:41:17 +0700

    add sidebar to user dashboard

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -23,53 +23,34 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
   </head>
   <body>
-    <div id="app">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-        <div class="container">
-          <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="{{ __('Toggle navigation') }}"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto"></ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-              <!-- Authentication Links -->
-              @guest @if (Route::has('login'))
+    <div class="container-fluid">
+      <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+          <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <span class="fs-5 d-none d-sm-inline">Dashboard</span>
+            </a>
+            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}"
-                  >{{ __('Login') }}</a
-                >
-              </li>
-              @endif @if (Route::has('register'))
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}"
-                  >{{ __('Register') }}</a
-                >
-              </li>
-              @endif @else
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  {{ Auth::user()->name }}
+                <a href="{{ route('home') }}" class="nav-link align-middle px-0">
+                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                 </a>
-
-                <ul
-                  class="dropdown-menu dropdown-menu-right"
-                  aria-labelledby="navbarDropdown"
-                >
+              </li>
+              
+              <li>
+                <a href="#" class="nav-link px-0 align-middle">
+                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Link 1</span> </a>
+              </li>
+            </ul>
+            <hr>
+            <div class="dropdown pb-4">
+              <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="d-none d-sm-inline mx-1">
+                {{ Auth::user()->name }}
+              </span>
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                <li>
                   <a
                     class="dropdown-item"
                     href="{{ route('logout') }}"
@@ -87,17 +68,18 @@
                   >
                     @csrf
                   </form>
-                </ul>
-              </li>
-              @endguest
-            </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </nav>
 
-      <main class="py-4">@yield('content')</main>
+        {{-- CONTENT AREA --}}
+        <div class="col py-3">
+          <main class="py-4">@yield('content')</main>
+        </div>
+      </div>
     </div>
-
     @yield('script')
   </body>
 </html>
