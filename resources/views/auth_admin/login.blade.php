7b228d9e695730c803594fa65d74a3535d786f72
Ryvel Timtothy Stamber <ryvel.timothy@gmail.com>
2021-09-20 01:18:07 +0700

    Admin UI

--- resources/views/auth_admin/login.blade.php
+++ resources/views/auth_admin/login.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
+  <div class="row mt-5 justify-content-center">
     <div class="col-md-8">
       <div class="card">
         <div class="card-header">{{ __('Login as Admin') }}</div>
         <div class="card-body">
           <form method="POST" action="{{ route('admin.login') }}">
             @csrf
-            <div class="form-group row">
+            <div class="form-group row mb-3">
               <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
               <div class="col-md-6">
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -40,9 +40,9 @@
                 </div>
               </div>
             </div>
-            <div class="form-group row mb-0">
+            <div class="form-group row mt-4 mb-0">
               <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" class="btn btn-primary text-white">
                 	{{ __('Login') }}
                 </button>
               </div>
---
+++ resources/views/pages/admins_dashboard/pubg/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+@section('content')
+  <div class="row">
+    <div class="col-sm">
+      <h1 class="fw-bold pb-1">NAMA PESERTA</h1><br>
+      <p class="text-muted"> NPP: XXXX-XXXX-XXXX |PUBG</p>
+    </div>
+    <div class="col-sm-auto align-self-center">
+      <a href="#" role="button" class="btn btn-danger fw-bold text-white"><i class="bi bi-person-x-fill"></i> Hapus Peserta</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg">
+      <div class="card bg-light my-3">
+        <div class="card-body">
+          <h5 class="card-title fw-bold">Edit Data Turnament</h5>
+          <div class="my-3">
+            <label for="exampleFormControlInput1" class="form-label">Jumlah Poin</label>
+            <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="1000">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="button" class="btn btn-success w-100 text-white fw-bold">Simpan Perubahan</button>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
