cd89e4efb19b585db526786f33cb7ff52b3bb305
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-10-03 14:22:30 +0700

    change time format for export data

--- app/Exports/RunDataExport.php
+++ app/Exports/RunDataExport.php
@@ -29,12 +29,6 @@ class RunDataExport implements FromCollection, WithHeadings, WithMapping
 
     public function collection()
     {
-        // $activities = DB::table('activities')
-        //                 ->join('users','users.id','=','activities.user_id')
-        //                 ->where('activity_type','Ride')
-        //                 ->select('users.name','activities.*')
-        //                 ->get()
-        //                 ->groupBy('user_id');
         $activities = Activity::where('activity_type','Run')
                         ->get()
                         ->sortBy('user_id');
@@ -48,9 +42,9 @@ class RunDataExport implements FromCollection, WithHeadings, WithMapping
             $activities->activity_name,
             $activities->activity_type,
             Carbon::parse($activities->activity_date)->format('d-m-Y'),
-            $activities->activity_length/1000,
-            $activities->activity_duration/60,
-            (($activities->activity_duration)/($activities->activity_length/1000))/60,
+            round(($activities->activity_length/1000), 2),
+            gmdate("H:i:s", $activities->activity_duration),
+            round(((($activities->activity_duration)/($activities->activity_length/1000))/60), 2),
             'https://www.strava.com/activities/'.$activities->strava_activity_id
         ];
     }
