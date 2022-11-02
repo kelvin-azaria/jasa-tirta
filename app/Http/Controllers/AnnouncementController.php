2e2eea5c19a278b702c5acb8b647d01d685ef5c5
Ryvel Stamber <ryvel.timothy@gmail.com>
2022-11-02 21:43:35 +0700

    remove unused function and generated comments

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -8,32 +8,11 @@ use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 class AnnouncementController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
       return DataTables::of(Announcement::query())->toJson();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(AnnouncementRequest $announcementRequest)
     {
       if($announcementRequest->type != 'note'){
@@ -56,12 +35,6 @@ class AnnouncementController extends Controller
       ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Announcement  $announcement
-     * @return \Illuminate\Http\Response
-     */
     public function show(Announcement $announcement)
     {
       return response()->json([
@@ -70,24 +43,6 @@ class AnnouncementController extends Controller
       ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Announcement  $announcement
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Announcement $announcement)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Announcement  $announcement
-     * @return \Illuminate\Http\Response
-     */
     public function update(AnnouncementRequest $announcementRequest, Announcement $announcement)
     {
       $announcement->update([
@@ -126,12 +81,6 @@ class AnnouncementController extends Controller
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Announcement  $announcement
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Announcement $announcement)
     {
       $announcement->delete();
